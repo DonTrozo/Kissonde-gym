df2685f2747bdcb7b4d4eb057141c51dc1326c7b
DonTrozo <trozo.antonio@gmail.com>
2026-08-27 09:23:53 +0100

    Export modular production screens

---
+++ src/screens/index.ts
@@ -0,0 +1,8 @@
+export { AccessScreen } from './AccessScreen';
+export { ClassesScreen } from './ClassesScreen';
+export { HomeScreen } from './HomeScreen';
+export { LoginScreen } from './LoginScreen';
+export { ProfileScreen } from './ProfileScreen';
+export { RewardsScreen } from './RewardsScreen';
+export { SupportScreen } from './SupportScreen';
+export { TrainScreen } from './TrainScreen';
